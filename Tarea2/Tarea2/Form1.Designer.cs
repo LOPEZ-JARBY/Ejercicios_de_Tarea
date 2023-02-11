fe7ca27f6d7065da0bdce63bccefff74a9b07f8d
LOPEZ-JARBY <123923309+LOPEZ-JARBY@users.noreply.github.com>
2023-02-11 02:50:35 -0600

    Calculo de  promedio
    Ejercicio que calcula el promedio final de un alumno, al tener 4 notas parciales.

--- Tarea2/Tarea2/Form1.Designer.cs
+++ Tarea2/Tarea2/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tarea2
 {
-    partial class Form1
+    partial class CalculoPromedio
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,149 @@ namespace Tarea2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Nota1textBox1 = new System.Windows.Forms.TextBox();
+            this.Nota2textBox2 = new System.Windows.Forms.TextBox();
+            this.Nota3textBox3 = new System.Windows.Forms.TextBox();
+            this.Nota4textBox4 = new System.Windows.Forms.TextBox();
+            this.PromediotextBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota1 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota2 : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota3 : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota4 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Promedio Final : ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Nota1textBox1
+            // 
+            this.Nota1textBox1.Location = new System.Drawing.Point(153, 33);
+            this.Nota1textBox1.Name = "Nota1textBox1";
+            this.Nota1textBox1.Size = new System.Drawing.Size(100, 20);
+            this.Nota1textBox1.TabIndex = 5;
+            this.Nota1textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Nota2textBox2
+            // 
+            this.Nota2textBox2.Location = new System.Drawing.Point(153, 64);
+            this.Nota2textBox2.Name = "Nota2textBox2";
+            this.Nota2textBox2.Size = new System.Drawing.Size(100, 20);
+            this.Nota2textBox2.TabIndex = 6;
+            // 
+            // Nota3textBox3
+            // 
+            this.Nota3textBox3.Location = new System.Drawing.Point(153, 102);
+            this.Nota3textBox3.Name = "Nota3textBox3";
+            this.Nota3textBox3.Size = new System.Drawing.Size(100, 20);
+            this.Nota3textBox3.TabIndex = 7;
+            // 
+            // Nota4textBox4
+            // 
+            this.Nota4textBox4.Location = new System.Drawing.Point(153, 137);
+            this.Nota4textBox4.Name = "Nota4textBox4";
+            this.Nota4textBox4.Size = new System.Drawing.Size(100, 20);
+            this.Nota4textBox4.TabIndex = 8;
+            // 
+            // PromediotextBox5
+            // 
+            this.PromediotextBox5.Location = new System.Drawing.Point(201, 242);
+            this.PromediotextBox5.Name = "PromediotextBox5";
+            this.PromediotextBox5.Size = new System.Drawing.Size(100, 20);
+            this.PromediotextBox5.TabIndex = 9;
+            this.PromediotextBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "CALCULARbutton1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CalculoPromedio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 334);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PromediotextBox5);
+            this.Controls.Add(this.Nota4textBox4);
+            this.Controls.Add(this.Nota3textBox3);
+            this.Controls.Add(this.Nota2textBox2);
+            this.Controls.Add(this.Nota1textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CalculoPromedio";
+            this.Text = "CalculoPromedio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Nota1textBox1;
+        private System.Windows.Forms.TextBox Nota2textBox2;
+        private System.Windows.Forms.TextBox Nota3textBox3;
+        private System.Windows.Forms.TextBox Nota4textBox4;
+        private System.Windows.Forms.TextBox PromediotextBox5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Tarea2/Tarea2/Form1.cs
+++ Tarea2/Tarea2/Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace Tarea2
 {
-    public partial class Form1 : Form
+    public partial class CalculoPromedio : Form
     {
-        public Form1()
+        Decimal PromedioFinal;
+        public CalculoPromedio()
         {
             InitializeComponent();
+
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Decimal Nota1, Nota2, Nota3, Nota4;
+
+            Nota1 = Decimal.Parse(Nota1textBox1.Text);
+            Nota2 = Decimal.Parse(Nota2textBox2.Text);
+            Nota3 = Decimal.Parse(Nota3textBox3.Text);
+            Nota4 = Decimal.Parse(Nota4textBox4.Text);
+
+            Calculo(Nota1, Nota2, Nota3, Nota4, PromedioFinal);
+
+        }
+        private Decimal Calculo(decimal Nota1, decimal Nota2, decimal Nota3, decimal Nota4, decimal Promedio)
+        {
+
+            Promedio = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+
+            PromediotextBox5.Text = Promedio.ToString();
+
+            return Promedio;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
